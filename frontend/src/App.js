2cefb96f87efb1e2ba2dbd7d1b76f01306999567
Deepak Acharya <dpakach@gmail.com>
2018-07-17 21:00:44 +0545

    Add custom navbar component with styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from "react";
-import BottomNavigationBar from "./NavbarButtom.js";
-import AddAlertBtn from "./home/AddAlertBtn";
-import Map from "./map/Map";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./home/Home";
+import React, {Component} from 'react';
+import BottomNavigationBar from './NavbarButtom.js';
+import AddAlertBtn from './home/AddAlertBtn';
+import Map from './map/Map';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './home/Home';
 
 export class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path="/" component={Home} />
-          <BottomNavigationBar />
+      <div className="container">
+        <div className="main-content">
+          <Router>
+            <div>
+              <Route exact path="/" component={Home} />
+              <BottomNavigationBar />
+            </div>
+          </Router>
         </div>
-      </Router>
+      </div>
     );
   }
 }
